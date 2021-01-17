c3819e6bd7d140a16c15dc3f85709c0bfa353637
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-16 21:28:20 -0300

    feat: add button signOut

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Container } from './styles';
 
 const Landing: React.FC = () => {
-  return <Container>LANDING</Container>;
+  const { signOut } = useAuth();
+
+  const signOutToProfile = useCallback(() => {
+    signOut();
+  }, [signOut]);
+
+  return (
+    <Container>
+      LANDING
+      <button type="button" onClick={signOutToProfile}>
+        Sair
+      </button>
+    </Container>
+  );
 };
 
 export default Landing;
